7eae6c2ac6c421112122449cb07485061b2170c9
Kitorosano <rosanoesteban2@gmail.com>
2022-03-08 23:31:37 -0300

    testing branch

--- src/main/java/uy/com/sofka/restaurante/comida/events/IngredienteAgregado.java
+++ src/main/java/uy/com/sofka/restaurante/comida/events/IngredienteAgregado.java
@@ -5,8 +5,14 @@ import uy.com.sofka.restaurante.comida.value.ComidaId;
 import uy.com.sofka.restaurante.comida.value.Descripcion;
 import uy.com.sofka.restaurante.comida.value.Nombre;
 import uy.com.sofka.restaurante.comida.value.Precio;
+import uy.com.sofka.restaurante.comida.value.CantidadIngrediente;
+import uy.com.sofka.restaurante.comida.value.IngredienteId;
 
-public class IngredienteAgregado {
+public class IngredienteAgregado extends DomainEvent{
 
+  public IngredienteAgregado() {
+    super("sofka.comida.agregaringrediente");
+    //TODO Auto-generated constructor stub
+  }
 
 }
